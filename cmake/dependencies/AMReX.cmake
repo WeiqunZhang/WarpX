f0d8feead36cfb7bee8d3f5b480f4a202c0b9b9a
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-04-10 12:57:32 -0700

    test geom

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -237,10 +237,10 @@ set(WarpX_amrex_src ""
     "Local path to AMReX source directory (preferred if set)")
 
 # Git fetcher
-set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
+set(WarpX_amrex_repo "https://github.com/WeiqunZhang/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "23.04"
+set(WarpX_amrex_branch "test_geom"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
--- run_test.sh
+++ run_test.sh
@@ -70,8 +70,8 @@ export SETUPTOOLS_USE_DISTUTILS="stdlib"
 python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
-git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 23.04 && cd -
+git clone https://github.com/WeiqunZhang/amrex.git
+cd amrex && git checkout --detach test_geom && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 
